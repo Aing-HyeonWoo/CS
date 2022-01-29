d78b4daa37d2e4bc2f82b44d0da0df86b78dde5b
Aing-HyeonWoo <77276416+Aing-HyeonWoo@users.noreply.github.com>
2022-01-30 04:11:56 +0900

    Add files via upload

---
+++ 077_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _077_class
+{
+
+    class AA
+    {
+        // 멤버 변수
+        int num1; // private
+        public int num2, num3;
+
+        //멤버 함수
+        public void Print()
+        {
+            Console.WriteLine("aa : {0}, bb : {0}, cc : {2} ", num1, num2, num3);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            // aa.num1 = 10; 오류
+            aa.num2 = 100;
+            aa.num3 = 100;
+
+            aa.Print();
+        }
+    }
+}
---
+++ 80_static/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _80_static
+{
+    class Program
+    {
+        class AA
+        {
+            public static int a;
+            public static int b;
+            public static readonly int c = 200;
+
+            public static void Print()
+            {
+                Console.WriteLine("a : {0}", a);
+                Console.WriteLine("b : {0}", b);
+            }
+        }
+
+        class BB
+        {
+            public int a;
+            public int b;
+
+            public void Print()
+            {
+                Console.WriteLine("a : {0}", a);
+                Console.WriteLine("b : {0}", b);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            AA.a = 10;
+            AA.b = 100;
+
+            AA.Print();
+
+            BB bb = new BB();
+            bb.a = 100;
+            bb.b = 200;
+            bb.Print();
+        }
+    }
+}
